e5edf48af8bbb85a2a925c6ff92b620639b93e00
Ygor Vieira <ygorus@gmail.com>
2023-06-27 13:32:34 -0300

    Singleton

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private static Singleton instancia = null;
+
+        public static Singleton GetInstancia
+        {
+            get
+            {
+                if (instancia == null)
+                {
+                    instancia = new Singleton();
+                    Console.WriteLine("Bola em jogo");
+                }
+                return instancia;
+            }
+        }
+
+        public void Mensagem(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
