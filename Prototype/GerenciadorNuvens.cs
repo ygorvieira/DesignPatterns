54dad33070e6477d221ef5056f12e283815a3c9a
Ygor Vieira <ygorus@gmail.com>
2023-06-28 19:40:12 -0300

    Prototype

---
+++ Prototype/GerenciadorNuvens.cs
@@ -0,0 +1,13 @@
+﻿namespace Prototype
+{
+    public class GerenciadorNuvens
+    {
+        public Dictionary<string, NuvemMolde> nuvens = new Dictionary<string, NuvemMolde>();
+
+        public NuvemMolde this[string key]
+        {
+            get { return nuvens[key]; }
+            set { nuvens.Add(key, value); }
+        }
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Permite a criação de novos objetos a partir de um modelo original ou protótipo que é clonado.
+            GerenciadorNuvens gerenciadorNuvens = new GerenciadorNuvens();
+
+            gerenciadorNuvens["padrão"] = new NuvemConcreta("branco", "azul");
+
+            gerenciadorNuvens["personalizada"] = new NuvemConcreta("branco", "laranja");
+
+            NuvemConcreta um = gerenciadorNuvens["padrão"].Clone() as NuvemConcreta;
+            NuvemConcreta dois = gerenciadorNuvens["padrão"].Clone() as NuvemConcreta;
+            NuvemConcreta tres = gerenciadorNuvens["personalizada"].Clone() as NuvemConcreta;
+
+            Console.ReadKey();
+        }
+    }
+}
