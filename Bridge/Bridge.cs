efb2eb780dac40390dc1ca4b8cbfcb5547f5f5f6
Ygor Vieira <ygorus@gmail.com>
2023-06-30 18:39:02 -0300

    Bridge

---
+++ Bridge/Bridge.cs
@@ -0,0 +1,12 @@
+﻿namespace Bridge
+{
+    public class Bridge
+    {
+        public IForma forma_solicitada {  get; set; }
+
+        public void ExibeQualFormaEstaDescendoNaTela()
+        {
+            Console.WriteLine(forma_solicitada.Descer());
+        }
+    }
+}
---
+++ Bridge/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Bridge
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Utilizado quando é desejável que uma interface (abstração) possa variar independentemente das suas implementações.
+            Bridge bridge = new Bridge();
+            Random random = new Random();
+
+            void Sortear()
+            {
+                if (random.Next(2) == 1)
+                    bridge.forma_solicitada = new Forma1();               
+                else
+                    bridge.forma_solicitada = new Forma2();
+
+                if (random.Next(1, 3) == 1)
+                    bridge.forma_solicitada.ICor = new Verde();
+                else if (random.Next(1, 3) == 2)
+                    bridge.forma_solicitada.ICor = new Laranja();
+                else
+                    bridge.forma_solicitada.ICor = new Rosa();
+            }
+
+            Console.WriteLine("Pressione ENTER para enviar uma forma");
+
+            while(1 > 0)
+            {
+                ConsoleKeyInfo input = Console.ReadKey();
+
+                if(input.KeyChar == 13)
+                    Sortear();
+
+                Console.WriteLine();
+
+                bridge.ExibeQualFormaEstaDescendoNaTela();
+            }
+        }
+    }
+}
