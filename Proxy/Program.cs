77da0f41b760008644a1fa09992a8f65d53bacb1
Ygor Vieira <ygorus@gmail.com>
2023-07-10 11:46:11 -0300

    Proxy

---
+++ Proxy/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Um Proxy, em sua forma mais geral, é uma classe que funciona como uma interface para outra coisa.
+
+            ProxyFase proxy = new ProxyFase();
+
+            Console.WriteLine("#### Tentando acessar a fase do jogo com Password incorreto ####");
+
+            Console.WriteLine(proxy.InformarPassword("batata"));
+
+            Console.WriteLine(proxy.Jogar());
+
+            Console.WriteLine();
+
+            Console.WriteLine("#### Tentando acessar a fase do jogo com Password correto ####");
+
+            Console.WriteLine(proxy.InformarPassword("IDDQD"));
+
+            Console.WriteLine(proxy.Jogar());
+
+            Console.WriteLine();
+
+            //Console.WriteLine("#### Usando o Proxy para controlar o acesso à fase do jogo ####");
+
+            //Console.WriteLine();
+
+            //ProxyFase proxy = new ProxyFase();
+
+            //Console.WriteLine(proxy.Jogar());
+
+            //Console.ReadKey();
+
+
+            //Console.WriteLine("#### Acessando a Fase do Jogo sem o Proxy ####");
+
+            //FaseJogo fase = new FaseJogo();
+
+            //Console.WriteLine(fase.Jogar());
+            //Console.WriteLine();
+
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ Proxy/ProxyFase.cs
@@ -0,0 +1,26 @@
+﻿namespace Proxy
+{
+    public class ProxyFase : IFase
+    {
+        FaseJogo fasejogo;
+        string password = "IDDQD";
+
+        public string Jogar()
+        {
+            if(this.fasejogo != null)
+                return fasejogo.Jogar();
+
+            return "Informe o PASSWORD correto para abrir a fase do jogo!";
+        }
+
+        public string InformarPassword(string codigo)
+        {
+            if(codigo == this.password)
+            {
+                this.fasejogo = new FaseJogo();
+                return "Passord correto!";
+            }
+            return "Password incorreto!";
+        }
+    }
+}
