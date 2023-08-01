f3717eecf3068def9bdf09bc8cc46207d8da4374
Ygor Vieira <ygorus@gmail.com>
2023-08-01 09:22:08 -0300

    Visitor

---
+++ Visitor/NivelVisitor.cs
@@ -0,0 +1,31 @@
+﻿namespace Visitor
+{
+    public class NivelVisitor : IVisitor
+    {
+        public void Identificar(Chefao chefao)
+        {
+            switch (chefao.NomeChefao) 
+            {
+                case "Boss 1":
+                    Console.WriteLine("O chefão {0} é {1}% difícil e tem {2} pontos de vida.", chefao.NomeChefao, 25, chefao.PontosVida);
+                    break;
+                case "Boss 2":
+                    Console.WriteLine("O chefão {0} é {1}% difícil e tem {2} pontos de vida.", chefao.NomeChefao, 50, chefao.PontosVida);
+                    break;
+            }
+        }
+
+        public void Identificar(FaseJogo fasejogo)
+        {
+            switch (fasejogo.NomeFase) 
+            {
+                case "Floresta":
+                    Console.WriteLine("A fase {0} no jogo é {1}% difícil.", fasejogo.NomeFase, 70);
+                    break;
+                case "Caverna":
+                    Console.WriteLine("A fase {0} no jogo é {1}% difícil.", fasejogo.NomeFase, 30);
+                    break;
+            }
+        }
+    }
+}
---
+++ Visitor/Program.cs
@@ -0,0 +1,21 @@
+﻿using Visitor;
+
+//Permite que se crie uma nova operação sem que se mude a classe dos elementos
+//sobre as quais ela opera.
+//É uma maneira de separar um algoritmo da estrutura de um objeto.
+
+List<IJogo> jogo = new List<IJogo>();
+
+jogo.Add(new FaseJogo() { NomeFase = "Floresta"});
+jogo.Add(new FaseJogo() { NomeFase = "Caverna" });
+jogo.Add(new Chefao() { NomeChefao = "Boss 1", PontosVida = 30 });
+jogo.Add(new Chefao() { NomeChefao = "Boss 2", PontosVida = 50 });
+
+NivelVisitor niveis = new NivelVisitor();
+
+foreach (var etapa in jogo)
+{
+    etapa.Visitante(niveis);
+}
+
+Console.ReadLine();
